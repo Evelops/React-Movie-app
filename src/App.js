b16d6e5d90186ee0d0da7f0f8f72f1231559595f
eomseungyeol <co6747@naver.com>
2021-07-20 20:21:14 +0900

    C:/Users/EomSeungYeol/Desktop/JavaScript/React/my-app/.git/

--- src/App.js
+++ src/App.js
@@ -1,35 +1,51 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Food({name,picture}) {
-  return <div>
-    <h2>I like {name}</h2>
-    <img src={picture}></img>
-  </div>
+function Food({name,picture,rating}) {
+   return (
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
+    </div>
+  );
 }
 
 const foodILike = [
   {
+    id:1,
     name: "kimchi",
-    image:"https://duckduckgo.com/?q=kimchi&t=h_&iax=images&ia=images"
+    image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.6Mea2CXE0T5R9pbPWubGUgHaEK%26pid%3DApi&f=1",
+    rating:5
   },
   {
+    id:2,
     name:"ramun",
-    image:"https://duckduckgo.com/?q=%EB%9D%BC%EB%A9%B4&t=h_&iar=images&iax=images&ia=images"
+    image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.CndlP20msq9LmKsnI2Q_wwHaE8%26pid%3DApi&f=1",
+    rating:4.9  
   },
   {
+    id:3,
     name:"bulgoggi",
-    image:"https://barefeetinthekitchen.com/beef-bulgogi-recipe/"
+    image:"https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Frecipe.ezmember.co.kr%2Fcache%2Frecipe%2F2016%2F12%2F30%2Fdf939769792632a48a0eba8bc895e8601.jpg&f=1&nofb=1",
+    rating:4.8
+  
   },
   {
+    id:4,
     name:"bap",
-    image:"https://dadonam.tistory.com/18"
+    image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.0EkTRtAUxtRkO1XirXRwMQHaEK%26pid%3DApi&f=1",
+    rating:4.7
+  
   },
-]
+];
 
 function App() {
   return (// 리엑트는 재사용 가능한 component를 만들고 값을 argument 로 전달할 수 있음. 
-    <div className="App">
-      {foodILike.map(dish => <Food name={dish.name} picture={dish.image}/>)}
+    <div>
+      {foodILike.map(dish => (
+        <Food rating={dish.rating} key={dish.id} name={dish.name} picture={dish.image}/>
+      ))}
     </div>// dish => 는 객체 !! foodILike가 가지고 있는 하나의 객체들을 의미함.
   );
 }
