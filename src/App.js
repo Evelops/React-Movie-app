983e0e206f650def551794ce6de4e67f2b9f0d64
eomseungyeol <co6747@naver.com>
2021-07-21 21:31:46 +0900

    C:/Users/EomSeungYeol/Desktop/JavaScript/React/my-app/.git/

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import React from 'react';
 import Prototypes from "prop-types";
 class App extends React.Component{
   state={ // state는 React.Componet에 들어 있는 녀셕임. 객체 라고 할 수 있음
-    count:0
+    isLoading:true,
+    movie:[]
   };
-  add=()=>{
-    this.setState(current=>({count:current.count+1}))
-  };
-  minus=()=>{
-    this.setState(current=>({count:current.count-1}))  
-  };
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({isLoading:false, book:true});
+    },6000);
+  }
+
   render(){
-    return <div>
-      <h1>I am a class Component: {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div>
+    const {isLoading}= this.state;
+    return <div>{isLoading ? "Loading..":"We are ready"}</div>
   }
 
   
