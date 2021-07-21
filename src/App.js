44fa09de5f55da6754ae63d94a6bc895fa4602aa
eomseungyeol <co6747@naver.com>
2021-07-21 22:11:00 +0900

    C:/Users/EomSeungYeol/Desktop/JavaScript/React/my-app/.git/

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import Prototypes from "prop-types";
+import axios from "axios";
+
 class App extends React.Component{
   state={ // state는 React.Componet에 들어 있는 녀셕임. 객체 라고 할 수 있음
     isLoading:true,
     movie:[]
   };
+
+  getMovies = async () =>{
+    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
+  };
   componentDidMount(){
-    setTimeout(()=>{
-      this.setState({isLoading:false, book:true});
-    },6000);
+    this.getMovies();
   }
 
   render(){
